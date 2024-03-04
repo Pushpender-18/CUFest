c7255ba7fd8dc24e7f4130699a3b45c68fe67dd0
Pushpender Singh <pushpender272320@gmail.com>
2024-03-05 00:00:36 +0530

    Configured riverpod

--- user/lib/main.dart
+++ user/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,); // Intializing Firebase 
 
-  runApp(const Placeholder(),);
+  runApp(const ProviderScope(child:Placeholder(),),);
 }
